b16afd9d46c5f58bbb7ca15086aa78b6c59e3edc
leondaki <leondaki@gmail.com>
2024-09-01 12:22:29 -0400

    Initial Commit

---
+++ Poker Tracker/Poker_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poker_TrackerApp.swift
+//  Poker Tracker
+//
+//  Created by Leonidas Kalpaxis on 9/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Poker_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
