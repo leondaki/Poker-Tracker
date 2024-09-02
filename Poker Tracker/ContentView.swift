1789725e1cc6b6fbfb151df787cac79968fe8af2
leondaki <leondaki@gmail.com>
2024-09-02 17:54:49 -0400

    create main player list view outline

--- Poker Tracker/ContentView.swift
+++ Poker Tracker/ContentView.swift
@@ -8,10 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    let players = [
+        Player(name: "Alice", money: 100),
+        Player(name: "Bob", money: 100),
+        Player(name: "Charles", money: 100),
+        Player(name: "Dave", money: 100)
+    ]
+    
     var body: some View {
-        VStack {
-            Text("Hello, Leonidas...")
-            Text("I think it is working!")
+        NavigationStack {
+            VStack {
+                Text("Pot: $100")
+                List(players) {
+                    player in PlayerRow(player: player)
+                }
+            }
+            .navigationTitle("Poker Tracker")
+        }
+    }
+}
+
+struct Player: Identifiable {
+    let id = UUID()
+    let name: String
+    let money: Int
+    
+    let spentThisRound: Int =  0
+}
+
+struct PlayerRow: View {
+    var player: Player
+    
+    var body: some View {
+        HStack {
+            HStack {
+                Text(player.name)
+                Spacer()
+            }
+  
+            Text("^ \(player.spentThisRound) ^")
+            
+            HStack {
+                Spacer()
+                Text("$\(player.money)")
+            }
         }
     }
 }
